cc0b33c570bcb7ce605cf09c5cf11c670ca50ba9
Nickmistretta9 <Nick@Nicks-MBP.home>
2017-08-08 21:37:32 -0400

    Render empty item if no notes

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+	return (
+		<div>
+			<h5>You Have No Notes</h5>
+			<p>Create A Note To Get Started</p>
+		</div>
+	);
+};
+
+export default NoteListEmptyItem;
